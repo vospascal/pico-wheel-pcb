b4cca3b423e2cc86f60c3f56c229d32bf9215e19
Pascal Vos <vospascal@gmail.com>
2023-10-29 01:00:02 +0200

    made functions

--- code.py
+++ code.py
@@ -1,4 +1,3 @@
-# Import necessary libraries/modules
 import board
 import digitalio
 import usb_hid
@@ -8,6 +7,20 @@ from hid_gamepad.simple import Gamepad
 
 # Initialize the Gamepad object using the HID (Human Interface Device) library
 gp = Gamepad(usb_hid.devices)
+
+
+########################################################################
+########################################################################
+
+# Function to handle button presses and releases
+def handle_buttons():
+    for i, button in enumerate(buttons):
+        gamepad_button_num = gamepad_buttons[i]
+        if button.value:
+            gp.release_buttons(gamepad_button_num)
+        else:
+            gp.press_buttons(gamepad_button_num)
+
 
 # Define the physical button pins
 button_pins = (board.GP11,)
@@ -23,6 +36,36 @@ for button in buttons:
     button.direction = digitalio.Direction.INPUT
     button.pull = digitalio.Pull.UP
 
+########################################################################
+########################################################################
+
+
+# Function to handle encoder rotation
+def handle_encoders():
+    for i, encoder in enumerate(encoders):
+        position = encoder.position
+        last_position = last_encoder_positions[i]
+
+        if position != last_position:
+            direction = "ClockWise" if position > last_position else "CounterClockWise"
+
+            gp.release_buttons(encoder_buttons[i][0])
+            gp.release_buttons(encoder_buttons[i][1])
+
+            if direction == "CounterClockWise":
+                gp.press_buttons(encoder_buttons[i][0])
+                time.sleep(0.1)
+
+            if direction == "ClockWise":
+                gp.press_buttons(encoder_buttons[i][1])
+                time.sleep(0.1)
+
+            gp.release_buttons(encoder_buttons[i][0])
+            gp.release_buttons(encoder_buttons[i][1])
+
+            last_encoder_positions[i] = position
+
+
 # Initialize four rotary encoders using specific pins
 enc_1 = rotaryio.IncrementalEncoder(board.GP9, board.GP10)
 enc_2 = rotaryio.IncrementalEncoder(board.GP13, board.GP14)
@@ -35,47 +78,12 @@ encoders = (enc_1, enc_2, enc_3, enc_4)
 encoder_buttons = [(2, 3), (4, 5), (6, 7), (8, 9)]
 
 # Initialize a list to store the last positions of the encoders
-last_positions = [0] * len(encoders)
+last_encoder_positions = [0] * len(encoders)
+##################################################
+########################################################################
+########################################################################
 
 # Main loop
 while True:
-    # Check the status of physical buttons
-    for i, button in enumerate(buttons):
-        gamepad_button_num = gamepad_buttons[i]
-        if button.value:
-            gp.release_buttons(gamepad_button_num)
-            # print(" release", gamepad_button_num)
-        else:
-            gp.press_buttons(gamepad_button_num)
-            # print(" press", gamepad_button_num)
-
-    # Read and print rotary encoder positions and toggle between buttons based on position
-    for i, encoder in enumerate(encoders):
-        position = encoder.position  # Read the current position of the encoder
-        last_position = last_positions[i]
-
-        # Check if the position has changed
-        if position != last_position:
-            # Determine the direction of rotation
-            direction = "ClockWise" if position > last_position else "CounterClockWise"
-            # print(f"Encoder {i + 1}: {position} ({direction})")
-
-            # Release both buttons associated with this encoder
-            gp.release_buttons(encoder_buttons[i][0])
-            gp.release_buttons(encoder_buttons[i][1])
-
-            if direction == "CounterClockWise":
-                gp.press_buttons(encoder_buttons[i][0])
-                time.sleep(0.1)
-
-            if direction == "ClockWise": 
-                gp.press_buttons(encoder_buttons[i][1])
-                time.sleep(0.1)
-
-            gp.release_buttons(encoder_buttons[i][0])
-            gp.release_buttons(encoder_buttons[i][1])
-
-            # Update the last known position for this encoder
-            last_positions[i] = position
-
-
+    handle_buttons()
+    handle_encoders()
